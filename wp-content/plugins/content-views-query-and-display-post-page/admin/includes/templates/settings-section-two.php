790854dcbf4bb9396f1df0a07e454cb24cd76465
easysofttech <tk@easysofttech.com>
2015-01-15 03:56:16 +0530

    added content view plugins for service and company pages

---
+++ wp-content/plugins/content-views-query-and-display-post-page/admin/includes/templates/settings-section-two.php
@@ -0,0 +1,57 @@
+<hr>
+
+<style>
+	#pt-cv-features {padding-right: 0; margin-bottom: 30px;}
+	#pt-cv-features * {font-size: 14px;}
+	#pt-cv-features h3 {font-size: 18px;}
+	#pt-cv-features .col-md-6 {padding: 0;}	
+</style>
+
+<h2>Want more powerful features?</h2>
+
+<div class="col-md-12" id="pt-cv-features">
+	<div class="col-md-6">
+		<h3>
+			More beautiful & awesome layouts
+		</h3>
+		<div>
+			<ul style="list-style: circle; margin-left: 20px;">
+				<li>Grid </li>
+				<li>Pinterest </li>
+				<li>Timeline </li>
+				<li>Scrollable/Slider list </li>
+				<li>Collapsible list</li>
+			</ul>
+		</div>
+	</div>
+	
+	<div class="col-md-6">
+		<h3>
+			Query custom post types
+		</h3>
+		<div>
+			<p>Content Views Pro allows you to query any custom post types.</p>
+			<p>And of course, It supports below well-known plugins:</p>			
+			<ul style="list-style: circle; margin-left: 20px;">
+				<li>WooCommerce</li>
+				<li>Easy Digital Downloads</li>
+				<li>Advanced Custom Fields</li>
+			</ul>
+		</div>
+	</div>
+	
+	<a style="font-size: 18px;" href="http://sample.contentviewspro.com/?utm_source=client&utm_medium=view" target="_blank">See all features</a>
+	<br>
+</div>
+
+<p style="margin-top: 20px;">
+	<a href="http://www.contentviewspro.com/pricing/?utm_source=settings_page&utm_medium=link&utm_campaign=content-views" target="_blank" class="btn btn-success">Upgrade now</a>
+	or
+	<a href="http://sample.contentviewspro.com/?utm_source=client&utm_medium=view" target="_blank" class="btn btn-info">Check demo site</a>
+</p>
+
+<hr>
+
+<p>I hope you enjoy with Content Views!</p>
+<p>Plugin developed by PT Guy (support@contentviewspro.com)</p>
+<p>Copyright &COPY; 2014</p>
